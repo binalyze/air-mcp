1fcb979e75f1fe3fc840356fdecd8b378c4678c4
Burak Kaan Özbek <burak.ozbek@binalyze.com>
2025-04-16 08:46:04 +0300

    Update version to 1.0.4

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import { organizationTools, ListOrganizationsArgsSchema } from './tools/organiza
 
 const server = new Server({
   name: 'air-mcp',
-  version: '1.0.3'
+  version: '1.0.4'
 }, {
   capabilities: {
     tools: {}
